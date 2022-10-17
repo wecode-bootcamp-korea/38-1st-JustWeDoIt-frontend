809418103cfa9b712484798bbdf56213b6b101aa
Eunji Kang <etothej@naver.com>
2022-10-18 00:14:48 +0900

    MODIFY: reset.scss 수정, router 재설정, Auth 컴포넌트 추가

--- src/Router.js
+++ src/Router.js
@@ -4,14 +4,23 @@ import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import SignUp from './pages/SignUp/SignUp';
 import SignIn from './pages/SignIn/SignIn';
+import Cart from './pages/Cart/Cart';
+import ProductFiltered from './pages/ProductFiltered/ProductFiltered';
+import './styles/reset.scss';
+import './styles/common.scss';
+import './styles/responsive.scss';
+import Nav from './components/Nav/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/card" element={<Cart />} />
         <Route path="/" element={<ProductList />} />
+        <Route path="/product/filtered/:number" element={<ProductFiltered />} />
         <Route path="/detail/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
