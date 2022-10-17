b63cf94a76bb7c1403f579fab6fd8c1f4eaf4ebd
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-10-17 20:28:00 +0900

    Router내용 수정, SignUp SignIn 페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductList from './pages/ProductList/ProductList';
-
-const hihi = 'jij';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/list" element={<ProductList />} />
+        <Route path="/detail:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <p>Nav</p>
+    </div>
+  );
+};
+export default SignUp;
