4f3e4795ddd6982295cc9339b6c6861f4322c934
Eunji Kang <etothej@naver.com>
2022-10-18 10:30:07 +0900

    ADD: nav 컴포넌트, 스타일 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Nav.scss';
+import { BsHeart } from 'react-icons/bs';
+import { BsPerson } from 'react-icons/bs';
+import { BsCart2 } from 'react-icons/bs';
+import { BsSearch } from 'react-icons/bs';
 
 const Nav = () => {
+  const [isShowing, setIsShowing] = useState(true);
+
+  const handleModal = () => {
+    setIsShowing(true);
+  };
+
   return (
-    <div>
-      <p>Nav</p>
-    </div>
+    <header className="topHeader">
+      <div className="headerWrapper">
+        <div className="topLogo">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/732/732084.png"
+            alt="logo"
+          />
+        </div>
+        <div className="navContainer">
+          <nav>
+            <div className="mobileNav">
+              <ul>
+                <li>Lifestyle</li>
+                <li>Jordan</li>
+                <li>Running</li>
+                <li>BasketBall</li>
+                <li>Soccer</li>
+              </ul>
+            </div>
+            <div className="desktopNav">
+              <ul>
+                <li>Lifestyle</li>
+                <li>Jordan</li>
+                <li>Running</li>
+                <li>BasketBall</li>
+                <li>Soccer</li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div className="btnGroup">
+          <ul>
+            <li className="searchBtn">
+              <BsSearch onClick={handleModal} />
+              {isShowing && (
+                <div className="searchModal">
+                  <div className="modalWrapper">
+                    <div className="modalTop">
+                      <div>
+                        <form className="searchForm">
+                          <input type="text" className="searchInput" />
+                          <button>찾기</button>
+                        </form>
+                      </div>
+                      <button className="closeBtn">Close</button>
+                    </div>
+                    <div className="searchSuggestions">
+                      <h4>추천 검색어</h4>
+                      <div>
+                        <ul>
+                          <li>Air Force 1</li>
+                          <li>Air Force 1</li>
+                          <li>Air Force 1</li>
+                          <li>Air Force 1</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </li>
+            <li className="likeBtn">
+              <BsHeart />
+            </li>
+            <li className="cartBtn">
+              <BsCart2 />
+            </li>
+            <li className="singInBtn">
+              <BsPerson />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
+
 export default Nav;
