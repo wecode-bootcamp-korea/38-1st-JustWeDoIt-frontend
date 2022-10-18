960f215f642de9272607f5e2e9f10a314ada6363
Jeong HyeonSeok <hyeonseok45@gmail.com>
2022-10-18 16:47:27 +0900

    footer 코멘트 부분 수정 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -44,9 +44,9 @@ const Footer = () => {
         <div className="footerCenterLeftItem">
           <MdOutlineWhereToVote className="where" />
           <div className="logoList">
-            <span>대한민국</span>
+            <span>WECODE</span>
             <div>
-              <span>© 2022 Nike, Inc. All Rights Reserved</span>
+              <span>© 2022 Just, WE All Rights </span>
             </div>
           </div>
         </div>
@@ -58,27 +58,20 @@ const Footer = () => {
         </div>
       </section>
       <section className="footerBottom">
-        <div className="footerBottomA">
+        <div className="footerBottomLeft">
           <div>
-            (유)나이키코리아 대표 Kimberlee Lynn Chang Mendes, 킴벌리 린 창
-            멘데스 | 서울 강남구 테헤란로 152 강남파이낸스센터 30층 |
-            통신판매업신고번호 2011-서울강남-03461 | 등록번호 220-88-09068사업자
-            정보 확인
+            (WE)WECODE 대표 JUST WE DO IT, | 서울특별시 강남구 테헤란로 427,
+            10층(삼성동) | 통신판매업신고번호 1111-1111-1111-1111 | 등록번호
+            999-99-99999사업자 정보 확인
           </div>
           <div>
-            고객센터 전화 문의 080-022-0182 FAX 02-6744-5880 | 이메일
-            service@nike.co.kr | 호스팅서비스사업자 (유)나이키코리아
+            고객센터 전화 문의 123-456-7889 FAX 02-1111-2222 | 이메일
+            wecode@gmail.com | 호스팅서비스사업자 (WE)WECODE
           </div>
         </div>
-        <div className="footerBottomB">
-          <div>
-            현금 등으로 결제 시 안전 거래를 위해 저희 쇼핑몰에서 가입한
-            파이서브코리아의 구매 안전 서비스(결제대금예치)를 이용하실 수
-            있습니다.
-          </div>
-          <div>
-            온라인디지털콘텐츠산업발전법에 의한 콘텐츠 보호 안내자세히 보기
-          </div>
+        <div className="footerBottomRight">
+          <div>JUST WE DO IT 팀은 보다 정확하고 깔끔한 코드를 추구합니다.</div>
+          <div>JUST WE DO IT</div>
         </div>
       </section>
     </footer>
