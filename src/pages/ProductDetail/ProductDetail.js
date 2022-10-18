ba2169e27079b68d010a4ce3269e05aea1d4274e
Eunji Kang <etothej@naver.com>
2022-10-19 00:36:38 +0900

    MODIFY: productDetail 수정 - mock data 추가

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { BsHeart } from 'react-icons/bs';
 import './ProductDetail.scss';
+import ProductDetaiBtn from 'components/ProductDetailBtn/ProductDetailBtn';
 
 const ProductDetail = () => {
-  const [product, setProduct] = useState(null);
+  // const [product, setProduct] = useState(null);
   const { id } = useParams();
   console.log(id);
 
@@ -43,46 +44,33 @@ const ProductDetail = () => {
                 139,000<span>원</span>
               </span>
             </div>
-
             <div className="productSize">
               <div className="sizeHeader">
                 <span>사이즈 선택</span>
               </div>
               <div className="sizeOptions">
-                <div className="sizeOption">
-                  <input type="radio" name="size" disabled />
-                  <label htmlFor="size">230</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">240</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">250</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">260</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">270</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">280</label>
-                </div>
-                <div className="sizeOption">
-                  <input type="radio" name="size" />
-                  <label htmlFor="size">290</label>
-                </div>
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
+                <ProductDetaiBtn />
               </div>
-              <div className="btnGroup">
+              <div className="productBtnGroup">
                 <button>장바구니</button>
                 <button>
                   위시리스트 <BsHeart />
                 </button>
+              </div>
+              <div className="productDescription">
+                <p>
+                  전설은 이렇게 만들어집니다. 나이키 포스 1 LE는 82년의 하드우드
+                  아이콘을 올 화이트 또는 올 블랙의 데일리 스타일로 재현합니다.
+                  내구성과 쿠셔닝, 착화감으로 아이들의 발을 클래식으로
+                  안내하세요.
+                </p>
+                <span className="productNumber">스타일: DH2925-111</span>
               </div>
             </div>
           </div>
