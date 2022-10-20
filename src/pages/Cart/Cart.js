b36947007153cc7a6d1e771a9cfcfcf01ce05a6c
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-10-20 14:31:08 +0900

    MockData위치 변경 및 fetch함수 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import { AiFillQuestionCircle } from 'react-icons/ai';
+import React, { useState, useEffect } from 'react';
 import CartItem from './CartItem';
 import CartSummary from './CartSummary';
 import './Cart.scss';
 
 const Cart = () => {
   const [cartData, setCartData] = useState([]);
+  useEffect(() => {
+    fetch('data/CartMockData.json')
+      .then(result => result.json())
+      .then(console.log)
+      .then(data => setCartData(data));
+  }, []);
 
   return (
     <div>
@@ -13,7 +18,7 @@ const Cart = () => {
         <div className="cartView">
           <div className="cartBodyWrap">
             <h4>장바구니</h4>
-            <CartItem />
+            <CartItem cartData={cartData} />
           </div>
           <CartSummary />
         </div>
