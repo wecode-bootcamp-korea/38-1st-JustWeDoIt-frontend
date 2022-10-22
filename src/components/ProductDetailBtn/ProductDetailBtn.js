b25966597e63b5f0fd760f4057c4acd1f4ddd150
Eunji Kang <etothej@naver.com>
2022-10-22 20:44:52 +0900

    MODIFY: 상세페이지 기능 구현 - id 값에 따른 상품 Fetch - 사이즈 버튼 컴포넌트에 데이터 추가 - productDetail.scss 파일 수정 (img 크기 변경)

--- src/components/ProductDetailBtn/ProductDetailBtn.js
+++ src/components/ProductDetailBtn/ProductDetailBtn.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './ProductDetailBtn.scss';
 import { useState, useEffect } from 'react';
 
-const ProductDetaiBtn = ({ size, selectedSize, selected }) => {
+const ProductDetaiBtn = ({ size, selectedSize, selected, value }) => {
   const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    if (size.stock === 0) {
+    if (value < 1) {
       setDisabled(true);
     } else {
       setDisabled(false);
@@ -23,7 +23,7 @@ const ProductDetaiBtn = ({ size, selectedSize, selected }) => {
         value={size.size}
       />
       <label htmlFor="size" ref={selected}>
-        {size.size}
+        {size}
       </label>
     </div>
   );
