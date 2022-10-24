6423ae635b49f22ead03a0d83e7b4cffd84639fa
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-10-24 12:56:45 +0900

    구현중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,32 +1,35 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import './Main.scss';
 
 const Main = () => {
   const [mainData, setMainData] = useState([]);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   fetch('/data/mainData.json')
     .then(response => response.json())
     .then(result => setMainData(result));
+  onClick = e => {
+    console.log(e);
+  };
 
   return (
     <div className="mainForm">
       {mainData.map(data => (
-        <div className="formBlock" key={data.id} id={data.id} onClick={onclick}>
-          {data.video ? (
-            <video src={data.video} loop autoPlay muted />
-          ) : (
-            <img src={data.image} />
-          )}
-          <div key={data.id}>
-            <Link to={data.url}></Link>
+        <div key={data.id} id={data.id}>
+          {/* <a href="#" onclick="location.href={}"> */}
+          <div className="formBlock" onClick={onclick}>
+            {data.video ? (
+              <video src={data.video} loop autoPlay muted />
+            ) : (
+              <img src={data.image} />
+            )}
+            <p className="title">{data.title} </p>
+            <p className="mainText">{data.mainText}</p>
+            <p className="mainText2">{data.mainText2}</p>
+            <button> 구매하기</button>
           </div>
-          <p className="title">{data.title} </p>
-          <p className="mainText">{data.mainText}</p>
-          <p className="mainText2">{data.mainText2}</p>
-          <button> 구매하기</button>
+          {/* </a> */}
         </div>
       ))}
     </div>
