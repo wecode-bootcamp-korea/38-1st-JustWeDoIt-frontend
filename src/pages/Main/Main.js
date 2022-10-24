e8e4eb972da6312c7e505f455e8510e962d3181d
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-10-24 13:14:53 +0900

    link 구현 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.scss';
 
 const Main = () => {
@@ -9,28 +10,26 @@ const Main = () => {
   fetch('/data/mainData.json')
     .then(response => response.json())
     .then(result => setMainData(result));
-  onClick = e => {
-    console.log(e);
-  };
 
   return (
     <div className="mainForm">
       {mainData.map(data => (
-        <div key={data.id} id={data.id}>
-          {/* <a href="#" onclick="location.href={}"> */}
-          <div className="formBlock" onClick={onclick}>
-            {data.video ? (
-              <video src={data.video} loop autoPlay muted />
-            ) : (
-              <img src={data.image} />
-            )}
-            <p className="title">{data.title} </p>
-            <p className="mainText">{data.mainText}</p>
-            <p className="mainText2">{data.mainText2}</p>
-            <button> 구매하기</button>
+        <Link to={data.url} key={data.id}>
+          <div key={data.id} id={data.id}>
+            <div className="formBlock">
+              {data.video ? (
+                <video src={data.video} loop autoPlay muted />
+              ) : (
+                <img src={data.image} />
+              )}
+              <p className="title">{data.title} </p>
+              <p className="mainText">{data.mainText}</p>
+              <p className="mainText2">{data.mainText2}</p>
+              <button> 구매하기</button>
+            </div>
+            {/* </a> */}
           </div>
-          {/* </a> */}
-        </div>
+        </Link>
       ))}
     </div>
   );
