ed87cf0fe72c43c3e147680206d58d103f79afa6
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-10-26 11:19:14 +0900

    payment layout

---
+++ src/pages/Payment/Payment.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import InputLabel from '../../components/Auth/Input/InputLabel';
+import Input from 'components/Auth/Input/Input';
+import './Payment.scss';
+
+const Payment = () => {
+  const [inputSet, setInputSet] = useState({
+    email: '',
+    password: '',
+  });
+  const saveInputSet = e => {
+    setInputSet({ ...inputSet, [e.target.name]: e.target.value });
+  };
+
+  const isEmailOkay =
+    inputSet.email.includes('@') > 0 &&
+    inputSet.email.lastIndexOf('.') < inputSet.email.length - 1 &&
+    inputSet.email.lastIndexOf('.') - inputSet.email.lastIndexOf('@') > 1;
+
+  return (
+    <div className="paymentContainer">
+      <p className="title">결제하기</p>
+      <div className="shippingOptions">
+        <p className="optionText">배송옵션</p>
+        <div className="paymentInputContainer">
+          <div className="nameContainer">
+            <Input
+              name="lastUserName"
+              saveInputSet={saveInputSet}
+              inputSet={inputSet}
+              type="text"
+              isEmailOkay={isEmailOkay}
+            />
+            <Input
+              name="firstName"
+              saveInputSet={saveInputSet}
+              inputSet={inputSet}
+              type="text"
+              isEmailOkay={isEmailOkay}
+            />
+          </div>
+          <div className="InputLabelContainer">
+            <InputLabel
+              name="address"
+              saveInputSet={saveInputSet}
+              inputSet={inputSet}
+              type="text"
+              isEmailOkay={isEmailOkay}
+            />
+          </div>
+          <div className="userInfoContainer">
+            <Input
+              name="phoneNumber"
+              saveInputSet={saveInputSet}
+              inputSet={inputSet}
+              type="text"
+              isEmailOkay={isEmailOkay}
+            />
+            <Input
+              name="email"
+              saveInputSet={saveInputSet}
+              inputSet={inputSet}
+              type="text"
+              isEmailOkay={isEmailOkay}
+            />
+          </div>
+          <div className="paymentbuttonContainer">
+            <button className="paymentStoragebutton">저장 및 계속</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
